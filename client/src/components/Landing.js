761a30af142a557ae03fb666631881fd56774879
Joshua Bernstein <school.josh@gmail.com>
2018-06-07 11:45:30 -0600

    Update Landing.js

--- client/src/components/Landing.js
+++ client/src/components/Landing.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Landing = () => {
   return (
     <div style={{ textAlign: 'center' }}>
-      <h1>Test Page </h1>
+      <h1>Test Page - 1 </h1>
     </div>
   );
 };
